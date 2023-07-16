ef9d965d56825b7f7255620cb2ca01b0d5f49a4d
liorbi24-meet <bitrany.lior@gmail.com>
2023-07-16 19:00:39 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,38 @@
+def create_youtube_video(title, description):
+	hashtags = []
+	for i in range(5):
+		hashtags.append(input("enter a hashtag: \n"))
+	return {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : {}, "hashtag" : hashtags}
+
+
+def like(youtube_video):
+	if "likes" in youtube_video:
+		youtube_video["likes"] += 1
+	return youtube_video
+
+def dislike(youtube_video):
+	if "dislikes" in youtube_video:
+		youtube_video["dislikes"] += 1
+	return youtube_video	
+
+def add_comment(youtubevideo, username, comment_text):
+	if "comments" in youtubevideo:
+		youtubevideo["comments"][username] = comment_text
+	return youtubevideo
+
+def similarity_to_video(vid1, vid2):
+	counter = 0
+	for i in vid1[hashtags]:
+		for j in vid2[hashtags]:
+			if i == j:
+				counter += 1
+	return str((counter / 5) * 100) + '%'  
+
+def main():
+	youtube_video = create_youtube_video("hello", "hi")
+	for i in range(456):
+		youtube_video = like(youtube_video)
+	print(youtube_video)
+
+if __name__ == "__main__":
+	main()
